d22ff89a12f84e32e2bc8c5fe3c994feef5aef9c
sarthak bansal <39374452+sarthak143@users.noreply.github.com>
2022-09-30 23:14:21 +0530

    local

--- src/App.js
+++ src/App.js
@@ -5,14 +5,13 @@ import Child from './child-parent-hooks/Child';
 import Parent from './child-parent-hooks/Parent';
 import ChildContext from './child-parent-context/ChildContext';
 import ParentContext from './child-parent-context/ParentContext';
-import ParentContext from './child-parent-context/ParentContext';
 import Navbar from './shared/Navbar';
 import { MainProvider } from './context/MainContext';
 export default function App() {
   return (
     <MainProvider>
       <BrowserRouter>
-        <div>
+        <div className="container-fluid">
           <Navbar />
           <Routes>
             <Route path="/" element={<Parent />} />
